52db59467677d0ceec36d6b193c6fe696512a96f
kangddong <rkdehddud96@naver.com>
2025-09-26 01:29:27 +0900

    [Feature] 스플래시 화면 구현

--- Hambug/HambugApp.swift
+++ Hambug/HambugApp.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 @main
 struct HambugApp: App {
+    @State private var appStateManager: AppStateManager = .init()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
+                .environment(appStateManager)
         }
+        
     }
 }
+
+fileprivate struct RootView: View {
+    @Environment(AppStateManager.self) var appStateManager
+    
+    var body: some View {
+        Group {
+            switch appStateManager.state {
+            case .splash:
+                SplashView()
+                
+            case .main:
+                ContentView()
+            }
+        }
+        .environment(appStateManager)
+    }
+}
+
