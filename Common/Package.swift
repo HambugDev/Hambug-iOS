c2af37a0f7a6a32303ef15115f10e67466c7f334
kangddong <rkdehddud96@naver.com>
2025-10-05 06:47:31 +0900

    [Core] DesignSystem+Color 컬러 분리, Color 생성 Command Plugins 추가

--- Common/Package.swift
+++ Common/Package.swift
@@ -13,10 +13,32 @@ let package = Package(
             name: "Managers",
             targets: ["Managers"]
         ),
+        .library(
+            name: "DesignSystem",
+            targets: ["DesignSystem"]
+        ),
     ],
     targets: [
         .target(
             name: "Managers"
+        ),
+        .target(
+            name: "DesignSystem",
+            resources: [
+                .process("Color.xcassets")
+            ]
+        ),
+        .plugin(
+            name: "ColorGenerator",
+            capability: .command(
+                intent: .custom(
+                    verb: "generate-colors",
+                    description: "Generate color constants from xcassets"
+                ),
+                permissions: [
+                    .writeToPackageDirectory(reason: "Generate DesignSystem+Color.swift file")
+                ]
+            )
         ),
     ]
 )
---
+++ Common/Sources/DesignSystem/DesignSystem+Color.swift
@@ -0,0 +1,24 @@
+import Foundation
+import SwiftUI
+
+public extension Color {
+    static let bgG100 = Color("bgG100", bundle: .module)
+    static let bgG200 = Color("bgG200", bundle: .module)
+    static let bgG50 = Color("bgG50", bundle: .module)
+    static let bgWhite = Color("bgWhite", bundle: .module)
+    static let bgYellow = Color("bgYellow", bundle: .module)
+    static let borderG300 = Color("borderG300", bundle: .module)
+    static let borderG400 = Color("borderG400", bundle: .module)
+    static let borderG500 = Color("borderG500", bundle: .module)
+    static let iconG600 = Color("iconG600", bundle: .module)
+    static let iconG800 = Color("iconG800", bundle: .module)
+    static let primaryGray = Color("primaryGray", bundle: .module)
+    static let primaryHambugRed = Color("primaryHambugRed", bundle: .module)
+    static let secondaryBrown = Color("secondaryBrown", bundle: .module)
+    static let secondaryGreen = Color("secondaryGreen", bundle: .module)
+    static let secondaryOrange = Color("secondaryOrange", bundle: .module)
+    static let secondaryYellow = Color("secondaryYellow", bundle: .module)
+    static let textG600 = Color("textG600", bundle: .module)
+    static let textG800 = Color("textG800", bundle: .module)
+    static let textG900 = Color("textG900", bundle: .module)
+}
