b7ec0b523919a6a735dfc19a673795d55475a84f
kangddong <rkdehddud96@naver.com>
2025-10-27 17:10:46 +0900

    [Feat] 네트워크 레이어 작성

--- Projects/Common/Network/Endpoint.swift
+++ Projects/Common/Network/Endpoint.swift
@@ -7,17 +7,45 @@
 
 import Foundation
 
-// MARK: - Base Endpoint Protocol
-protocol Endpoint {
-    var baseURL: String { get }
-    var path: String { get }
-    var method: HTTPMethod { get }
-    var headers: [String: String]? { get }
-    var queryParameters: [String: Any]? { get }
+// MARK: - HTTP Method
+enum HTTPMethod: String {
+  case GET = "GET"
+  case POST = "POST"
+  case PUT = "PUT"
+  case DELETE = "DELETE"
+  case PATCH = "PATCH"
 }
 
-// MARK: - Request Protocol
-protocol APIRequest: Codable {
-    associatedtype Response: Codable
-    var endpoint: any Endpoint { get }
+// MARK: - Base Endpoint Protocol
+protocol Endpoint {
+  var baseURL: String { get }
+  var path: String { get }
+  var method: HTTPMethod { get }
+  var headers: [String: String] { get }
+  var queryParameters: [String: Any] { get }
+  var body: Data? { get }
+}
+
+extension Endpoint {
+  func createURL() -> URL? {
+    var urlComponents = URLComponents(string: baseURL.appending(path))
+    var queryItems = [URLQueryItem]()
+    
+    queryParameters.forEach {
+      queryItems.append(URLQueryItem(name: $0.key, value: "\($0.value)"))
+    }
+    urlComponents?.queryItems = queryItems
+    
+    return urlComponents?.url
+  }
+  
+  func createURLRequest() throws -> URLRequest {
+    guard let url = createURL() else { throw NetworkError.invalidURL }
+    var request = URLRequest(url: url)
+    request.httpMethod = method.rawValue
+    var allHeaders: [String: String] = [:]
+    headers.forEach { allHeaders.updateValue($1, forKey: $0) }
+    
+    return request
+  }
 }
--- Projects/Common/Network/NetworkServiceInterface.swift
+++ Projects/Common/Network/NetworkServiceInterface.swift
@@ -10,36 +10,5 @@ import Combine
 
 // MARK: - Network Interface Protocol
 protocol NetworkServiceInterface {
-  func request<T: APIRequest>(_ request: T) -> AnyPublisher<T.Response, NetworkError>
-}
-
-// MARK: - API Endpoint Protocol
-protocol APIEndpoint {
-    var baseURL: String { get }
-    var path: String { get }
-    var method: HTTPMethod { get }
-    var headers: [String: String]? { get }
-    var parameters: [String: Any]? { get }
-    var body: Data? { get }
-}
-
-// MARK: - HTTP Method
-enum HTTPMethod: String {
-    case GET = "GET"
-    case POST = "POST"
-    case PUT = "PUT"
-    case DELETE = "DELETE"
-    case PATCH = "PATCH"
-}
-
-// MARK: - Response Wrapper
-struct APIResponse<T: Codable>: Codable {
-    let success: Bool
-    let data: T?
-    let message: String?
-    let code: Int?
-}
-
-struct ErrorResponse: Decodable {
-  let message: String
+  func request<T: Decodable>(_ endpoint: any Endpoint, responseType: T.Type) -> AnyPublisher<T, NetworkError>
 }
